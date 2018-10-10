2d6f0a4129e3c60c2572cd23ac97df5f85f933dd
Dana Palmer <roiai4@hotmail.com>
2018-10-10 03:03:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(array)
-  array.each do |element|
-    puts element.capitalize
+  array.split.map do |element|
+    puts element.capitalize.join(" ")
   end
 end
