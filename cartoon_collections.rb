501dd52ec4d5afe6e61a008d707c8842d9d7377b
Dana Palmer <roiai4@hotmail.com>
2018-10-10 02:58:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index(1) {|name, index| print "#{index}. #{name}"}
+  dwarves.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
 end
