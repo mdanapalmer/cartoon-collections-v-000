959300d6421ded548f212088e9a2de0ad668c8bb
Dana Palmer <roiai4@hotmail.com>
2018-10-10 03:01:21 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,4 +2,8 @@ def roll_call_dwarves(dwarves)
   dwarves.each.with_index(1) {|name, i| print "#{i}. #{name}"}
 end
 
-def summon_captain_planet()
+def summon_captain_planet(array)
+  array.collect do |element|
+    puts element
+  end
+end
