082fe8b3d7c4ea7e2969369783a55aecbfb4d991
Dana Palmer <roiai4@hotmail.com>
2018-10-10 03:22:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,5 +11,5 @@ def long_planeteer_calls(calls_long)
 end
 
 def find_the_cheese(cheddar_cheese)
-  cheddar_cheese.find? { |cheese| cheese == "cheddar"}
+  cheddar_cheese.find { |cheese| cheese == "cheddar"}
 end
