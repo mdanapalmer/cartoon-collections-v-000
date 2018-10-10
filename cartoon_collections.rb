6540da7345c5df27d3a58aad595cf310b7659496
Dana Palmer <roiai4@hotmail.com>
2018-10-10 03:19:48 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,5 +7,5 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls_long)
-  calls_long.map { |long| if long.length > 4 }
+  calls_long.any? { |long| if long.length > 4 }
 end
